9621ea6229699dab904f3ff2bb5bd2477e94b8f7
John-Khalil <johnadelkhalil@gmail.com>
2023-01-12 01:17:31 +0200

    extended port class created

---
+++ Grbl_Esp32/src/custom/extendedPort.h
@@ -0,0 +1,16 @@
+#ifndef extendedPort_h
+#define extendedPort_h
+
+#include <stdint.h>
+#include <cstdlib>
+#include <functional>
+#include <vector>
+
+
+class extendedPort{
+
+};
+
+extern extendedPort spiPort;
+
+#endif
